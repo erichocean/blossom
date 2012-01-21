9d60c936afe29101868a69e94e25f8a3ccefba8f
Erich Ocean <erich.ocean@me.com>
2012-01-20 16:15:34 -0800

    removed unused CSS, rhtml, and images; removed english.lproj from foundation

--- desktop/english.lproj/strings.js
+++ desktop/english.lproj/strings.js
@@ -1,3 +1,17 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.stringsFor('English', {
+  '_SC.DateTime.dayNames': 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
+  '_SC.DateTime.abbreviatedDayNames': 'Sun Mon Tue Wed Thu Fri Sat',
+  '_SC.DateTime.monthNames': 'January February March April May June July August September October November December',
+  '_SC.DateTime.abbreviatedMonthNames': 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'
+}) ;
+
 // ========================================================================
 // Sprout Core
 // copyright 2006-2011 Strobe Inc.
