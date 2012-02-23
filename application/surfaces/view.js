9f3fb90ed601ea41aca7e84556b40f5953644543
Erich Ocean <erich.ocean@me.com>
2012-02-23 15:42:14 -0800

    reorganise SC.View (still needs a lot of work)

--- application/surfaces/view.js
+++ application/surfaces/view.js
@@ -37,6 +37,87 @@ var white =    "white";
 */
 SC.View = SC.LeafSurface.extend({
 
+  // ..........................................................
+  // PSURFACE SUPPORT
+  //
+
+  __tagName__: 'canvas',
+
+  __useContentSize__: true, // we need our width and height attributes set
+
+  // ..........................................................
+  // RENDERING SUPPORT
+  //
+
+  updateLayout: function() {
+    // console.log('SC.View#updateLayout()', SC.guidFor(this));
+    var benchKey = 'SC.ViewSurface#updateLayout()';
+    SC.Benchmark.start(benchKey);
+
+    var layer = this.getPath('contentView.layer');
+    if (layer) layer.updateLayout();
+
+    SC.Benchmark.end(benchKey);
+  },
+
+  updateDisplay: function() {
+    console.log('SC.View#updateDisplay()', SC.guidFor(this));
+    var benchKey = 'SC.ViewSurface#updateDisplay()',
+        updateKey = 'SC.ViewSurface#updateDisplay() - update',
+        copyKey = 'SC.ViewSurface#updateDisplay() - copy';
+    SC.Benchmark.start(benchKey);
+
+    // sc_assert(document.getElementById(this.__sc_element__.id));
+    var layer = this.get('layer');
+
+    // SC.Benchmark.start(updateKey);
+    // if (layer) layer.updateDisplay();
+    // SC.Benchmark.end(updateKey);
+
+    // var ctx = this.getPath('layer.context'),
+    var psurface = SC.psurfaces[this.__id__],
+        canvas = psurface? psurface.__element__ : null,
+        ctx = canvas? canvas.getContext('2d') : null,
+        w = canvas.width, h = canvas.height;
+
+    sc_assert(ctx);
+
+    // Draw background.
+    ctx.fillStyle = base3;
+    ctx.fillRect(0, 0, w, h);
+    // ctx.strokeStyle = base0;
+    // ctx.lineWidth = 2; // overlap of 1 on the inside
+    // ctx.strokeRect(0, 0, ctx.width, ctx.height);
+
+    SC.Benchmark.start(copyKey);
+    // if (layer) layer.copyIntoContext(ctx);
+    SC.Benchmark.end(copyKey);
+
+    // Draw lines overlay.
+    ctx.beginPath();
+    var line = h/2;
+    if (h%2 === 0) line += 0.5;
+    ctx.moveTo(0, line);
+    ctx.lineTo(w, line);
+    var vline = w/2;
+    if (w%2 === 0) vline += 0.5;
+    ctx.moveTo(vline, 0);
+    ctx.lineTo(vline, h);
+    ctx.strokeStyle = orange;
+    ctx.lineWidth = 0.5;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(w/2, h/2, 3, 0, 2*Math.PI, false);
+    ctx.fillStyle = orange;
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(w/2, h/2, 15, 0, 2*Math.PI, false);
+    ctx.lineWidth = 0.5;
+    ctx.stroke();
+
+    SC.Benchmark.end(benchKey);
+  }
+
   // bounds: function() {
   //   // FIXME!
   //   return this.getPath('container.bounds');
@@ -123,87 +204,6 @@ SC.View = SC.LeafSurface.extend({
   //   return hitLayer? hitLayer.get('view') : this ;
   // },
 
-  // ..........................................................
-  // RENDERING SUPPORT
-  //
-
-  updateLayout: function() {
-    // console.log('SC.View#updateLayout()', SC.guidFor(this));
-    var benchKey = 'SC.ViewSurface#updateLayout()';
-    SC.Benchmark.start(benchKey);
-
-    var layer = this.getPath('contentView.layer');
-    if (layer) layer.updateLayout();
-
-    SC.Benchmark.end(benchKey);
-  },
-
-  updateDisplay: function() {
-    console.log('SC.View#updateDisplay()', SC.guidFor(this));
-    var benchKey = 'SC.ViewSurface#updateDisplay()',
-        updateKey = 'SC.ViewSurface#updateDisplay() - update',
-        copyKey = 'SC.ViewSurface#updateDisplay() - copy';
-    SC.Benchmark.start(benchKey);
-
-    // sc_assert(document.getElementById(this.__sc_element__.id));
-    // var layer = this.getPath('contentView.layer');
-
-    // SC.Benchmark.start(updateKey);
-    // if (layer) layer.updateDisplay();
-    // SC.Benchmark.end(updateKey);
-
-    // var ctx = this.getPath('layer.context'),
-    var psurface = SC.psurfaces[this.__id__],
-        canvas = psurface? psurface.__element__ : null,
-        ctx = canvas? canvas.getContext('2d') : null,
-        w = canvas.width, h = canvas.height;
-
-    sc_assert(ctx);
-
-    // Draw background.
-    ctx.fillStyle = base3;
-    ctx.fillRect(0, 0, w, h);
-    // ctx.strokeStyle = base0;
-    // ctx.lineWidth = 2; // overlap of 1 on the inside
-    // ctx.strokeRect(0, 0, ctx.width, ctx.height);
-
-    SC.Benchmark.start(copyKey);
-    // if (layer) layer.copyIntoContext(ctx);
-    SC.Benchmark.end(copyKey);
-
-    // Draw lines overlay.
-    ctx.beginPath();
-    var line = h/2;
-    if (h%2 === 0) line += 0.5;
-    ctx.moveTo(0, line);
-    ctx.lineTo(w, line);
-    var vline = w/2;
-    if (w%2 === 0) vline += 0.5;
-    ctx.moveTo(vline, 0);
-    ctx.lineTo(vline, h);
-    ctx.strokeStyle = orange;
-    ctx.lineWidth = 0.5;
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(w/2, h/2, 3, 0, 2*Math.PI, false);
-    ctx.fillStyle = orange;
-    ctx.fill();
-    ctx.beginPath();
-    ctx.arc(w/2, h/2, 15, 0, 2*Math.PI, false);
-    ctx.lineWidth = 0.5;
-    ctx.stroke();
-
-    SC.Benchmark.end(benchKey);
-  },
-
-  // ..........................................................
-  // DOM SUPPORT (Private, Browser-only)
-  //
-
-  __tagName__: 'canvas',
-
-  __useContentSize__: true, // we need our width and height attributes set
-
   // initElement: function() {
   //   arguments.callee.base.apply(this, arguments);
   //   var element = this.__sc_element__;
@@ -214,140 +214,140 @@ SC.View = SC.LeafSurface.extend({
   // VIEWPORT SUPPORT
   //
 
-  /**
-    The SC.Layer subclass to instantiate to create this view's layer.
+  // /**
+  //   The SC.Layer subclass to instantiate to create this view's layer.
+  // 
+  //   @property {SC.Layer}
+  // */
+  // layerClass: SC.Layer,
+  // 
+  // layer: function(key, value) {
+  //   sc_assert(value === undefined); // We're read only.
+  //   return this._sc_layer;
+  // }.property(),
+  // 
+  // hitTestLayer: null,
+  // 
+  // hitTestLayer: function(key, value) {
+  //   sc_assert(value === undefined); // We're read only.
+  //   return this._sc_hitTestLayer;
+  // }.property(),
 
-    @property {SC.Layer}
-  */
-  layerClass: SC.Layer,
-
-  layer: function(key, value) {
-    sc_assert(value === undefined); // We're read only.
-    return this._sc_layer;
-  }.property(),
-
-  hitTestLayer: null,
-
-  hitTestLayer: function(key, value) {
-    sc_assert(value === undefined); // We're read only.
-    return this._sc_hitTestLayer;
-  }.property(),
-
-  createSurface: function() {
-    // console.log('SC.View#createSurface()');
-
-    // For now, just do this one time.
-    if (this._sc_didCreateSurface) return;
-    this._sc_didCreateSurface = true;
-
-    arguments.callee.base.apply(this, arguments);
-    var container = this;
-
-    // SC.ViewSurface only has two layers: `layer` and `hitTestLayer`.
-    var K = this.get('layerClass');
-    sc_assert(K && K.kindOf(SC.Layer));
-
-    // We want to allow the developer to provide a layout hash on the view, 
-    // or to override the 'layout' computed property.
-    if (this.hasOwnProperty('layout')) {
-      // It's still possible that layout is a computed property. Don't use 
-      // `get()` to find out!
-      var layout = this.layout;
-      if (typeof layout === "object") {
-        // We assume `layout` is a layout hash. The layer will throw an 
-        // exception if `layout` is invalid -- don't test for that here.
-        this._sc_layer = K.create({
-          layout: layout,
-          owner: this, // TODO: Do we need owner here?
-          container: container,
-          delegate: this
-        });
-        this._sc_hitTestLayer = K.create({
-          layout: layout,
-          isHitTestOnly: true,
-          owner: this, // TODO: Do we need owner here?
-          container: container,
-          delegate: this
-        });
-      } else {
-        this._sc_layer = K.create({
-          // `layout` is whatever the default on SC.Layer is
-          owner: this, // TODO: Do we need owner here?
-          container: container,
-          delegate: this
-        });
-        this._sc_hitTestLayer = K.create({
-          // `layout` is whatever the default on SC.Layer is
-          isHitTestOnly: true,
-          owner: this, // TODO: Do we need owner here?
-          container: container,
-          delegate: this
-        });
-      }
-
-      // Only delete layout if it is not a computed property. This allows 
-      // the computed property on the prototype to shine through.
-      if (typeof layout !== "function" || !layout.isProperty) {
-        // console.log('deleting layout');
-        delete this.layout;
-      }
-    } else {
-      this._sc_layer = K.create({
-        // `layout` is whatever the default on SC.Layer is
-        owner: this, // TODO: Do we need owner here?
-        container: container,
-        delegate: this
-      });
-      this._sc_hitTestLayer = K.create({
-        // `layout` is whatever the default on SC.Layer is
-        isHitTestOnly: true,
-        owner: this, // TODO: Do we need owner here?
-        container: container,
-        delegate: this
-      });
-    }
-
-    this.notifyPropertyChange('layer');
-    this.notifyPropertyChange('hitTestLayer');
-  },
-
-  destroySurface: function() {
-    console.log('SC.ViewSurface#destroySurface()');
-  },
+  // createSurface: function() {
+  //   // console.log('SC.View#createSurface()');
+  // 
+  //   // For now, just do this one time.
+  //   if (this._sc_didCreateSurface) return;
+  //   this._sc_didCreateSurface = true;
+  // 
+  //   arguments.callee.base.apply(this, arguments);
+  //   var container = this;
+  // 
+  //   // SC.ViewSurface only has two layers: `layer` and `hitTestLayer`.
+  //   var K = this.get('layerClass');
+  //   sc_assert(K && K.kindOf(SC.Layer));
+  // 
+  //   // We want to allow the developer to provide a layout hash on the view, 
+  //   // or to override the 'layout' computed property.
+  //   if (this.hasOwnProperty('layout')) {
+  //     // It's still possible that layout is a computed property. Don't use 
+  //     // `get()` to find out!
+  //     var layout = this.layout;
+  //     if (typeof layout === "object") {
+  //       // We assume `layout` is a layout hash. The layer will throw an 
+  //       // exception if `layout` is invalid -- don't test for that here.
+  //       this._sc_layer = K.create({
+  //         layout: layout,
+  //         owner: this, // TODO: Do we need owner here?
+  //         container: container,
+  //         delegate: this
+  //       });
+  //       this._sc_hitTestLayer = K.create({
+  //         layout: layout,
+  //         isHitTestOnly: true,
+  //         owner: this, // TODO: Do we need owner here?
+  //         container: container,
+  //         delegate: this
+  //       });
+  //     } else {
+  //       this._sc_layer = K.create({
+  //         // `layout` is whatever the default on SC.Layer is
+  //         owner: this, // TODO: Do we need owner here?
+  //         container: container,
+  //         delegate: this
+  //       });
+  //       this._sc_hitTestLayer = K.create({
+  //         // `layout` is whatever the default on SC.Layer is
+  //         isHitTestOnly: true,
+  //         owner: this, // TODO: Do we need owner here?
+  //         container: container,
+  //         delegate: this
+  //       });
+  //     }
+  // 
+  //     // Only delete layout if it is not a computed property. This allows 
+  //     // the computed property on the prototype to shine through.
+  //     if (typeof layout !== "function" || !layout.isProperty) {
+  //       // console.log('deleting layout');
+  //       delete this.layout;
+  //     }
+  //   } else {
+  //     this._sc_layer = K.create({
+  //       // `layout` is whatever the default on SC.Layer is
+  //       owner: this, // TODO: Do we need owner here?
+  //       container: container,
+  //       delegate: this
+  //     });
+  //     this._sc_hitTestLayer = K.create({
+  //       // `layout` is whatever the default on SC.Layer is
+  //       isHitTestOnly: true,
+  //       owner: this, // TODO: Do we need owner here?
+  //       container: container,
+  //       delegate: this
+  //     });
+  //   }
+  // 
+  //   this.notifyPropertyChange('layer');
+  //   this.notifyPropertyChange('hitTestLayer');
+  // },
+  // 
+  // destroySurface: function() {
+  //   console.log('SC.ViewSurface#destroySurface()');
+  // },
 
   // ..........................................................
   // CONTENT VIEW SUPPORT
   //
 
-  contentView: null,
-
-  _sc_contentView: null,
-  _sc_contentViewDidChange: function() {
-    // console.log('SC.ViewSurface#_sc_contentViewDidChange()');
-    var old = this._sc_contentView,
-        cur = this.get('contentView'),
-        layer = this.get('layer');
-
-    if (cur && cur.isViewClass) {
-      this.set('contentView', cur.create());
-      return;
-    }
-
-    sc_assert(old === null || old.kindOf(SC.View), "Blossom internal error: SC.Application^_sc_contentView is invalid.");
-    sc_assert(cur === null || cur.kindOf(SC.View), "SC.Application@ui must either be null or an SC.View instance.");
-
-    if (old === cur) return; // Nothing to do.
-
-    if (old) old.set('surface', null);
-    if (cur) cur.set('surface', this);
-
-    this.displayDidChange();
-  }.observes('contentView')
+  // contentView: null,
+  // 
+  // _sc_contentView: null,
+  // _sc_contentViewDidChange: function() {
+  //   // console.log('SC.ViewSurface#_sc_contentViewDidChange()');
+  //   var old = this._sc_contentView,
+  //       cur = this.get('contentView'),
+  //       layer = this.get('layer');
+  // 
+  //   if (cur && cur.isViewClass) {
+  //     this.set('contentView', cur.create());
+  //     return;
+  //   }
+  // 
+  //   sc_assert(old === null || old.kindOf(SC.View), "Blossom internal error: SC.Application^_sc_contentView is invalid.");
+  //   sc_assert(cur === null || cur.kindOf(SC.View), "SC.Application@ui must either be null or an SC.View instance.");
+  // 
+  //   if (old === cur) return; // Nothing to do.
+  // 
+  //   if (old) old.set('surface', null);
+  //   if (cur) cur.set('surface', this);
+  // 
+  //   this.displayDidChange();
+  // }.observes('contentView')
 
 });
 
 // HACK: FIXME
-SC.LAYOUT_AUTO = 'auto';
-SC._VIEW_DEFAULT_DIMS = 'marginTop marginLeft'.w();
+// SC.LAYOUT_AUTO = 'auto';
+// SC._VIEW_DEFAULT_DIMS = 'marginTop marginLeft'.w();
 
 } // BLOSSOM
