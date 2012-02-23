ed607891ae711dd2447cceaf624a0a773858f7b2
Erich Ocean <erich.ocean@me.com>
2012-02-22 20:42:17 -0800

    tweak weighting so that choices 9 and 10 can happen

--- examples/psurfaces_test/core.js
+++ examples/psurfaces_test/core.js
@@ -23,7 +23,7 @@ var surface = SC.View.create({
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
     ctx.fillText("Welcome to the SC.Psurface fuzz tester.", ctx.width/2, (ctx.height/2)-180);
-    ctx.fillText("The surface tree  is being repeatedly modified, and then", ctx.width/2, (ctx.height/2)-100);
+    ctx.fillText("The surface tree is being repeatedly modified, and then", ctx.width/2, (ctx.height/2)-100);
     ctx.fillText("the corresponding Psurface and rendering tree (DOM) is", ctx.width/2, (ctx.height/2)-60);
     ctx.fillText("being updated and exhaustively verified for correctness.", ctx.width/2, (ctx.height/2)-20);
     ctx.fillText("Click anywhere to end the fuzz test.", ctx.width/2, (ctx.height/2)+60);
@@ -279,8 +279,8 @@ function moveChild(composite, child) {
 function modifyTree() {
   var node, leaf, composite;
   // debugger;
-  var weight = Math.floor(Math.random()*11);
-  weight = Math.floor(Math.random()*weight);
+  var weight = Math.floor(Math.random()*13); // Max 12
+  weight = Math.floor(Math.random()*weight); // Max 11
   switch (Math.floor(Math.random()*weight)) {
     case 0:
       composite = fetchComposite(null, false);
