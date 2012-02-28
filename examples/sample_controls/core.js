2ca66f943841a66fc88d08aa20b0510d4c53b986
Erich Ocean <erich.ocean@me.com>
2012-02-28 13:55:30 -0800

    tweaked behavior of bottomRightSurface (clicks now change color, no dragging)

--- examples/sample_controls/core.js
+++ examples/sample_controls/core.js
@@ -85,113 +85,39 @@ function main() {
           canvas = psurface? psurface.__element__ : null,
           ctx = canvas? canvas.getContext('2d') : null,
           w = canvas.width, h = canvas.height;
-  
-      // console.log('w',w,'h',h);
-  
+
       ctx.save();
-  
+
       // Clear background.
       ctx.clearRect(0, 0, w, h);
-  
+
       // Draw fps meter.
       ctx.fillStyle = green;
       ctx.font = "16pt Calibri";
       ctx.textBaseline = "middle";
       ctx.textAlign = "center";
-      ctx.fillText("(drag me)", w/2, 50);
+      ctx.fillText("(click me)", w/2, 50);
       ctx.fillText(SC.Benchmark.fps(), w/2, h/2);
-
-      var timingFunction = this.get('timingFunction');
-      if (timingFunction) {
-        ctx.fillText(timingFunction, w/2, h-50);
-      }
 
       ctx.restore();
     },
 
-    mouseDown: function(evt) {
-      // console.log('pane#mouseDown');
-      this._clientX = evt.clientX;
-      this._clientY = evt.clientY;
-      this.set('opacity', 0.5);
-      var transform = this.get('transform'),
-          frame = this.get('frame');
-      transform = SC.Transform3DRotateX(transform, Math.random()*(Math.PI/3));
-      transform = SC.Transform3DRotateY(transform, Math.random()*(Math.PI/3));
-      transform = SC.Transform3DScale(transform, (Math.random()*2)+0.5, (Math.random()*2)+0.5);
-      // transform = SC.Transform3DTranslate(transform, (Math.random()*frame.x/10), (Math.random()*frame.y/10));
-      this.set('transform', transform);
-      return true;
-    },
-
-    mouseDragged: function(evt) {
-      // console.log('pane#mouseDragged');
-      SC.AnimationTransaction.begin({ duration: 0 });
-      var frame = this.get('frame');
-      frame.x = frame.x + evt.clientX - this._clientX;
-      frame.y = frame.y + evt.clientY - this._clientY;
-      this._clientX = evt.clientX;
-      this._clientY = evt.clientY;
-      SC.AnimationTransaction.end();
-      return true;
-    },
-
-    mouseUp: function(evt) {
-      // console.log('pane#mouseUp');
-      SC.AnimationTransaction.begin({ duration: 0 });
-      this.doingMouseUp = true;
-      var frame = this.get('frame');
-      frame.x = frame.x + evt.clientX - this._clientX;
-      frame.y = frame.y + evt.clientY - this._clientY;
-      this.set('frame', frame);
-      this.doingMouseUp = false;
-      delete this._clientX;
-      delete this._clientY;
-      SC.AnimationTransaction.end();
-      this.set('opacity', 1.0);
+    click: function(evt) {
+      // console.log('pane#click');
       var color = "rgb(%@,%@,%@)".fmt(Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255));
       this.set('backgroundColor', color);
       color = "rgb(%@,%@,%@)".fmt(Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255));
       this.set('borderColor', color);
       this.set('borderWidth', Math.floor(Math.random()*16));
       this.set('cornerRadius', Math.floor(Math.random()*31));
-
-      var sz = SC.app.computeViewportSize();
-      frame.x = Math.random()*(sz.width-frame.width);
-      frame.y = Math.random()*(sz.height-frame.height);
-
-      var transform = this.get('transform');
-      SC.SetIdentityTransform3D(transform);
-      transform = SC.Transform3DRotateZ(transform, Math.random()*(Math.PI/3)*(Math.random() > 0.5? 1 : -1));
-      this.set('transform', transform);
-
-      this.shiftKey = evt.shiftKey;
       return true;
-    },
-
-    transitionForKey: function(key) {
-      if (this.doingMouseUp && key === 'frame') {
-        var keys = Object.keys(SC.TimingFunction), name;
-        keys = keys.filter(function(key) { return key !== 'get'; });
-        name = keys[Math.floor(Math.random()*keys.length)];
-        this.set('timingFunction', name);
-        return SC.TransitionAnimation.create({
-          duration: this.shiftKey? 2000 : 400,
-          timingFunction: SC.TimingFunction.get(name)
-        });
-      }
     }
   });
   
-  // var frame = SC.MakeRect(50, 50, 300, 200);
-  // pane.set('frame', frame);
-  // pane.set('backgroundColor', "white");
-  // pane.set('borderColor', "black");
-  // pane.set('borderWidth', 1);
-  // pane.set('cornerRadius', 5);
-  // pane.set('zIndex', 1);
-  // 
-  // SC.app.get('surfaces').add(pane);
+  pane.set('backgroundColor', "white");
+  pane.set('borderColor', "black");
+  pane.set('borderWidth', 1);
+  pane.set('cornerRadius', 5);
 
   var split = SC.SplitSurface.create({
     topLeftSurface: surface,
