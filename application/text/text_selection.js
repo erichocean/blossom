34ac2de7fefe2baf18506089cd347a2d63bb7b2f
Erich Ocean <erich.ocean@me.com>
2012-03-02 19:55:47 -0800

    Implemented SC.TextSurface.

--- application/text/text_selection.js
+++ application/text/text_selection.js
@@ -2,8 +2,44 @@
 // Project:   SproutCore Costello - Property Observing Library
 // Copyright: ©2006-2011 Strobe Inc. and contributors.
 //            Portions ©2008-2010 Apple Inc. All rights reserved.
-// License:   Licensed under MIT license (see license.js)
+//            Code within if (BLOSSOM) {} sections is ©2012 Fohr Motion 
+//            Picture Studios. All rights reserved.
+// License:   Most code licensed under MIT license (see SPROUTCORE-LICENSE).
+//            Code within if (BLOSSOM) {} sections is under GPLv3 license
+//            (see BLOSSOM-LICENSE).
 // ==========================================================================
+/*globals BLOSSOM sc_assert */
+
+if (BLOSSOM) {
+
+SC.TextSelection = function(start, end) {
+  this.start = start;
+  this.end = end;
+
+  sc_assert(this.isValid);
+
+  return this;
+};
+
+SC.TextSelection.prototype.__defineGetter__('length', function() {
+  return this.end - this.start;
+});
+
+SC.TextSelection.prototype.__defineGetter__('isValid', function() {
+  var start = this.start,
+      end = this.end;
+
+  if (typeof start === 'number' && Math.floor(start) === start &&
+      typeof end === 'number' && Math.floor(end) === end &&
+      0 <= start && start <= end)
+  {
+    return true;
+  } else return false;
+});
+
+} // BLOSSOM
+
+if (! BLOSSOM) {
 
 /**
   @class
@@ -101,3 +137,5 @@ SC.TextSelection = SC.Object.extend(SC.Copyable, SC.Freezable,
   }
 
 }) ;
+
+} // ! BLOSSOM
