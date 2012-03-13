54f4e0173d178a394616f1607531a072cd10d768
Erich Ocean <erich.ocean@me.com>
2012-03-12 20:16:26 -0700

    stub SC.KeyframeAnimation

---
+++ application/animations/keyframe.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// Project:   Blossom - Modern, Cross-Platform Application Framework
+// Copyright: ©2012 Fohr Motion Picture Studios. All rights reserved.
+// License:   Licensed under the GPLv3 license (see BLOSSOM-LICENSE).
+// ==========================================================================
+/*globals sc_assert */
+
+sc_require('animations/animation');
+
+SC.KeyframeAnimation = SC.Animation.extend({
+
+  isKeyframeAnimation: true // Walk like a duck.
+
+});
