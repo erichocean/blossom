f2432ccd7f4bb7410e8b8cd9243e045ed5c62204
Erich Ocean <erich.ocean@me.com>
2012-03-12 18:51:24 -0700

    also remove cassowary tests

--- tests/constraints/SimplexSolver-test.js
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 1998-2000 Greg J. Badros
-// Use of this source code is governed by the LGPL, which can be found in the
-// COPYING.LGPL file.
-//
-// Parts Copyright (C) 2011, Alex Rusell (slightlyoff@chromium.org)
-
-s = new c.SimplexSolver();
-print(s);
